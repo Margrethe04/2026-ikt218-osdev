96d485fa8eee5a687bac9bcf1c2dc8dcd2043cb6
Margrethe Krogelien <margrethek@uia.no>
2026-02-04 13:53:15 +0000

    Assignment 3: interrupts (IDT/ISR/IRQ/keyboard)

--- src/OSDev_50/src/kernel.c
+++ src/OSDev_50/src/kernel.c
@@ -1,6 +1,9 @@
 #include <libc/stdint.h>
 #include "gdt.h"
 #include "terminal.h"
+#include <descriptor_tables.h>
+#include <interrupts.h>
+#include <keyboard.h>
 
 void main(uint32_t mb_magic, uint32_t mb_info_addr)
 {
@@ -11,6 +14,17 @@ void main(uint32_t mb_magic, uint32_t mb_info_addr)
 
     terminal_initialize();
     terminal_write("Hello World\n");
+
+    init_idt();
+    init_irq();
+    isr_register_default_prints();
+    keyboard_init();
+
+    __asm__ volatile ("int $0x0");
+    __asm__ volatile ("int $0x3");
+    __asm__ volatile ("int $0x4");
+
+    __asm__ volatile ("sti");
 
     for (;;) {
         __asm__ volatile ("hlt");
