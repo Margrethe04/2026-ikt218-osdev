a9398b3150e4c940d52db2f24f56e869376225c1
Margrethe Krogelien <margrethek@uia.no>
2026-02-23 17:21:19 +0000

    Assignment6

--- src/OSDev_50/src/song.c
+++ src/OSDev_50/src/song.c
@@ -4,7 +4,6 @@
 #include "common.h"
 #include "libc/stdio.h"
 
-// Default song data (kan byttes ut med dine store arrays)
 Note music_1[] = {
     {E5, 200}, {R, 80}, {E5, 200}, {R, 80}, {C5, 200}, {E5, 200},
     {G5, 300}, {R, 120}, {G4, 300}, {R, 200}
@@ -13,17 +12,7 @@ Note music_1[] = {
 uint32_t music_1_len = sizeof(music_1) / sizeof(Note);
 
 void enable_speaker(){
-    // Read the current state of the PC speaker control register
     uint8_t speaker_state = inb(PC_SPEAKER_PORT);
-    /*
-    Bit 0: Speaker gate
-            0: Speaker disabled
-            1: Speaker enabled
-    Bit 1: Speaker data
-            0: Data is not passed to the speaker
-            1: Data is passed to the speaker
-    */
-    // Check if bits 0 and 1 are not set (0 means that the speaker is disabled)
     if (speaker_state != (speaker_state | 3)) {
         // If bits 0 and 1 are not set, enable the speaker by setting bits 0 and 1 to 1
         outb(PC_SPEAKER_PORT, speaker_state | 3);
@@ -43,18 +32,18 @@ void play_sound(uint32_t frequency) {
 
     uint16_t divisor = (uint16_t)(PIT_BASE_FREQUENCY / frequency);
 
-    // Set up the PIT
+    //Set up the PIT
     outb(PIT_CMD_PORT, 0b10110110);
     outb(PIT_CHANNEL2_PORT, (uint8_t)(divisor & 0xFF));
     outb(PIT_CHANNEL2_PORT, (uint8_t)(divisor >> 8));
 
-    // Enable the speaker by setting bits 0 and 1
+    //Enable the speaker by setting bits 0 and 1
     uint8_t speaker_state = inb(PC_SPEAKER_PORT);
     outb(PC_SPEAKER_PORT, speaker_state | 0x03);
 }
 
 void stop_sound(){
-    // Stop the sound by disabling the gate to the speaker
+    //Stop the sound by disabling the gate to the speaker
     uint8_t speaker_state = inb(PC_SPEAKER_PORT);
     outb(PC_SPEAKER_PORT, speaker_state & ~0x03); // Clear bits 0 and 1
 }
@@ -81,7 +70,6 @@ void play_song(Song *song) {
     play_song_impl(song);
 }
 
-// Kernel calls this
 void play_music(void)
 {
     Song s;
