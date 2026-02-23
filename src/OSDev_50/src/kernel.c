7bcd101b89f94899ecce21707ac4604674990489
Margrethe Krogelien <margrethek@uia.no>
2026-02-23 11:23:07 +0000

    Assignment 4: memory management and PIT

--- src/OSDev_50/src/kernel.c
+++ src/OSDev_50/src/kernel.c
@@ -1,9 +1,13 @@
 #include <libc/stdint.h>
+#include <libc/stdio.h>
+
 #include "gdt.h"
 #include "terminal.h"
-#include <descriptor_tables.h>
-#include <interrupts.h>
-#include <keyboard.h>
+#include "descriptor_tables.h"
+#include "interrupts.h"
+#include "keyboard.h"
+#include "memory.h"
+#include "pit.h"
 
 extern uint32_t end;
 
@@ -12,10 +16,11 @@ void main(uint32_t mb_magic, uint32_t mb_info_addr)
     (void)mb_magic;
     (void)mb_info_addr;
 
-    gdt_init();
-
+    
     terminal_initialize();
     terminal_write("Hello World\n");
+
+    gdt_init();
 
     init_idt();
     init_irq();
@@ -23,13 +28,38 @@ void main(uint32_t mb_magic, uint32_t mb_info_addr)
     isr_register_default_prints();
     keyboard_init();
 
-    __asm__ volatile ("int $0x0");
-    __asm__ volatile ("int $0x3");
-    __asm__ volatile ("int $0x4");
+    init_kernel_memory(&end);
+    init_paging();
+
+    printf("Before malloc:\n");
+    print_memory_layout();
+
+    void* a = malloc(1000);
+    void* b = malloc(2000);
+
+    printf("After malloc:\n");
+    print_memory_layout();
+
+    free(a);
+    free(b);
+
+    printf("After free:\n");
+    print_memory_layout();
+
+    init_pit();
 
     __asm__ volatile ("sti");
 
-    for (;;) {
-        __asm__ volatile ("hlt");
-    }
+    int counter = 0;
+
+    for (int i = 0; i < 5; i++) {
+        printf("[%d] busy sleep 1s\n", counter++);
+        sleep_busy(1000);
+
+        printf("[%d] interrupt sleep 1s\n", counter++);
+        sleep_interrupt(1000); }
+
+        printf("Done. Halting...\n");
+            while (1) {
+            __asm__ volatile ("hlt"); }
 }
